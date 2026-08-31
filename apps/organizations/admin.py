72dc05ad030bf6ae31f4da4c381d6cd2f982976f
Adityagupta-18 <aditya.work018@gmail.com>
2026-08-31 23:44:07 +0530

    Added Organization model

--- apps/organizations/admin.py
+++ apps/organizations/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apps.organizations.models import Organization
 
 # Register your models here.
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'organization_type', 'owner', 'verification_status','created_at')
+    list_filter = ('organization_type', 'verification_status')
+    search_fields = ('name', 'owner__username', 'owner__email')
--- apps/organizations/models.py
+++ apps/organizations/models.py
@@ -1,3 +1,27 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+
+class Organization(models.Model):
+    ORGANIZATION_TYPE_CHOICES = [
+        ('MED', 'Medical'),
+        ('FIRE_RESCUE', 'Fire Rescue'),
+        ('POLICE_SECURITY', 'Police/Security'),
+        ('NGO', 'Non-Governmental Organization')
+    ]
+    VERIFICATION_STATUS_CHOICES = [
+        ('PEND', 'Pending'),
+        ('VERIF', 'Verified'),
+        ('REJ', 'Rejected'),
+    ]
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    organization_type = models.CharField(max_length=20, choices=ORGANIZATION_TYPE_CHOICES)
+    owner = models.OneToOneField('users.User', on_delete=models.CASCADE, related_name='organization')
+    address = models.TextField()
+    verification_document = models.FileField(upload_to='verification_documents/')
+    verification_status = models.CharField(max_length=5, choices=VERIFICATION_STATUS_CHOICES, default='PEND')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
