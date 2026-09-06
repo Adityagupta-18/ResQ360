0bed5c4d4407cf5594597085061db9f76ac12ec0
Adityagupta-18 <aditya.work018@gmail.com>
2026-09-06 20:24:11 +0530

    assigned incident to org

---
+++ apps/incidents/services.py
@@ -0,0 +1,15 @@
+from apps.organizations.models import Organization
+from django.utils import timezone
+from .models import IncidentOrganization
+
+def dispatch_incident(incident):
+    required_types = incident.get_required_organization_types()
+    queryset=Organization.objects.filter(organization_type__in=required_types,verification_status='VERIF')
+
+    for organization in queryset:
+        IncidentOrganization.objects.create(
+            incident=incident,
+            organization=organization,
+            status="NOTIFIED",
+            notified_at=timezone.now(),
+        )
