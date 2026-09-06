f8861ca4936f7927a9f60249bc567e85d45d828c
Adityagupta-18 <aditya.work018@gmail.com>
2026-09-06 18:39:32 +0530

    added tracking/emergency view

--- apps/incidents/urls.py
+++ apps/incidents/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IncidentCreateView
+from .views import IncidentCreateView , IncidentTrackingView
 
 urlpatterns = [
     path('',IncidentCreateView.as_view(), name='incident-create'),
+    path('track/<emergency_id>/',IncidentTrackingView.as_view(), name='track-incident'),
 ]
--- apps/incidents/views.py
+++ apps/incidents/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .serializers import IncidentSerializer
+from .serializers import IncidentSerializer , IncidentTrackingSerializer
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Incident
 
 class IncidentCreateView(APIView):
     permission_classes=[AllowAny,]
     
     def post(self,request):
         serializer=IncidentSerializer(data=request.data)
-        
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data , status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class IncidentTrackingView(APIView):
+    permission_classes=[AllowAny,]
+
+    def get(self,request,emergency_id):
+        incident=get_object_or_404(Incident,emergency_id=emergency_id)
+        serializer=IncidentTrackingSerializer(incident)
+        return Response(serializer.data,status=status.HTTP_200_OK)  
