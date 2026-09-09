d76d059cba211f24f6b0759beac52a9f88178fab
Adityagupta-18 <aditya.work018@gmail.com>
2026-09-10 00:10:02 +0530

    Making org change the incident status

--- static/js/emergency-detail.js
+++ static/js/emergency-detail.js
@@ -3,7 +3,13 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     const data = await apiRequest(`/incidents/track/${emergencyId}/`);
 
+    const incidents = await apiRequest("/organizations/incidents/");
+    const incident = incidents.find(
+    item => item.emergency_id === emergencyId
+    );
+
     console.log("Emergency details:", data);
+    console.log("Assignment:", incident);
 
     document.getElementById("incidentTitle").textContent =
         `${data.incident_type} — ${data.emergency_id}`;
@@ -13,4 +19,17 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     document.getElementById("incidentLocation").textContent =
         data.location_address;
+
+    
+    const enrouteBtn = document.getElementById("enrouteBtn");
+
+    enrouteBtn.addEventListener("click", async function () {
+        const response = await apiRequest(
+            `/incidents/organization-assignments/${incident.id}/enroute/`,
+            {
+                method: "POST"
+            }
+        );
+        console.log("Enroute response:", response);
+    });
 });
