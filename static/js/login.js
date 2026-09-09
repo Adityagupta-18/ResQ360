869ae3b131d4e364294ea677e4ed80d4bfef7613
Adityagupta-18 <aditya.work018@gmail.com>
2026-09-09 22:42:37 +0530

    Added access and refresh token to local storage

--- static/js/login.js
+++ static/js/login.js
@@ -1,12 +1,26 @@
-
 const loginForm = document.getElementById("loginForm");
 
-loginForm.addEventListener("submit", function(event) {
+loginForm.addEventListener("submit", async function(event) {
     event.preventDefault();
 
     const email = document.getElementById("LoginEmail").value;
     const password = document.getElementById("LoginPassword").value;
 
-    console.log("Email:", email);
-    console.log("Password:", password);
+    const data = await apiRequest("/auth/login/", {
+        method: "POST",
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    });
+
+    console.log("Login response:", data);
+    saveTokens(data.access, data.refresh);
 });
+
+
+
+function saveTokens(access, refresh) {
+    localStorage.setItem("resq360_access_token", access);
+    localStorage.setItem("resq360_refresh_token", refresh);
+}
