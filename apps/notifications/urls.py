11d2ac66699374e511e3818037f5f58e6279d6e6
Adityagupta-18 <aditya.work018@gmail.com>
2026-09-12 18:09:25 +0530

    Added serializer for notification

--- apps/notifications/urls.py
+++ apps/notifications/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.notifications.views import NotificationReadView
+from apps.notifications.views import NotificationReadView , NotificationListView
 
 
 urlpatterns = [
+    path("", NotificationListView.as_view(), name="notification-list"),
     path("<uuid:notification_id>/read/",NotificationReadView.as_view(),name="notification-read"),
 ]
--- apps/notifications/views.py
+++ apps/notifications/views.py
@@ -1,9 +1,12 @@
+from django.db import models
 from django.shortcuts import render
 from rest_framework.views import APIView
 from apps.notifications.models import Notification
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from .serializers import NotificationSerializer
 
 # Create your views here.
 
@@ -25,3 +28,16 @@ class NotificationReadView(APIView):
             {"error": "You are not the recipient of this notification."},
             status=status.HTTP_403_FORBIDDEN
         )
+
+
+class NotificationListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        notifications = Notification.objects.filter(
+            models.Q(organization__owner=request.user) |
+            models.Q(volunteer=request.user)
+        ).order_by("-created_at")
+
+        serializer = NotificationSerializer(notifications, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
